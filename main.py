da73a7287a3e1f9223673f336b286661a4ebc459
Thiago da Silva <thiago180244@sou.faccat.br>
2026-06-02 13:05:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from modules.ping_test import ping_host
 from modules.dns_lookup import dns_lookup
 from modules.port_check import check_port
+from modules.ip_info import get_local_ip, get_public_ip
 
 from colorama import Fore, Style, init
 
@@ -10,13 +11,18 @@ init(autoreset=True)
 while True:
 
     # Header
-    print(Fore.CYAN + Style.BRIGHT + "\n=== NETWORK DIAGNOSTIC TOOL ===\n")
+    print(Fore.CYAN + Style.BRIGHT)
+    print("=" * 35)
+    print("   NETWORK DIAGNOSTIC TOOL")
+    print("=" * 35)
 
-    # Menu options
-    print(Fore.GREEN + "1 - Ping Test")
-    print(Fore.GREEN + "2 - DNS Lookup")
-    print(Fore.GREEN + "3 - Port Check")
-    print(Fore.RED + "4 - Exit")
+    # Menu
+    print(Fore.GREEN + "\n1 - Ping Test")
+    print("2 - DNS Lookup")
+    print("3 - Port Check")
+    print("4 - Show Local IP")
+    print("5 - Show Public IP")
+    print(Fore.RED + "6 - Exit")
 
     # User choice
     choice = input(Fore.YELLOW + "\nSelect an option: ")
@@ -24,9 +30,11 @@ while True:
     # Ping Test
     if choice == "1":
 
-        host = input(Fore.YELLOW + "\nEnter the IP or domain: ")
+        host = input(
+            Fore.YELLOW + "\nEnter the IP address or domain: "
+        )
 
-        print(Fore.CYAN + "\nRunning ping test...\n")
+        print(Fore.CYAN + "\nRunning Ping Test...\n")
 
         result = ping_host(host)
 
@@ -35,9 +43,11 @@ while True:
     # DNS Lookup
     elif choice == "2":
 
-        domain = input(Fore.YELLOW + "\nEnter the domain: ")
+        domain = input(
+            Fore.YELLOW + "\nEnter the domain name: "
+        )
 
-        print(Fore.CYAN + "\nRunning DNS lookup...\n")
+        print(Fore.CYAN + "\nRunning DNS Lookup...\n")
 
         result = dns_lookup(domain)
 
@@ -46,37 +56,74 @@ while True:
     # Port Check
     elif choice == "3":
 
-        host = input(Fore.YELLOW + "\nEnter the host: ")
+        host = input(
+            Fore.YELLOW + "\nEnter the host: "
+        )
 
         try:
 
-            port = int(input(Fore.YELLOW + "Enter the port: "))
+            port = int(
+                input(
+                    Fore.YELLOW + "Enter the port number: "
+                )
+            )
 
             print(Fore.CYAN + "\nChecking port status...\n")
 
             result = check_port(host, port)
 
-            # Green if open
-            if "OPEN" in result:
+            if "OPEN" in result.upper():
                 print(Fore.GREEN + result)
 
-            # Red if closed
-            elif "CLOSED" in result:
+            elif "CLOSED" in result.upper():
                 print(Fore.RED + result)
 
             else:
                 print(Fore.WHITE + result)
 
         except ValueError:
-            print(Fore.RED + "\nInvalid port. Please enter a valid number.")
+            print(
+                Fore.RED +
+                "\nInvalid port. Please enter a valid number."
+            )
 
-    # Exit
+    # Local IP
     elif choice == "4":
 
-        print(Fore.YELLOW + "\nClosing Network Diagnostic Tool...\n")
+        print(Fore.CYAN + "\nGetting local IP information...\n")
+
+        result = get_local_ip()
+
+        print(Fore.WHITE + result)
+
+    # Public IP
+    elif choice == "5":
+
+        print(Fore.CYAN + "\nGetting public IP information...\n")
+
+        result = get_public_ip()
+
+        print(Fore.WHITE + result)
+
+    # Exit
+    elif choice == "6":
+
+        print(
+            Fore.YELLOW +
+            "\nClosing Network Diagnostic Tool...\n"
+        )
 
         break
 
-    # Invalid option
+    # Invalid Option
     else:
-        print(Fore.RED + "\nInvalid option. Please try again.\n")
+
+        print(
+            Fore.RED +
+            "\nInvalid option. Please try again.\n"
+        )
+
+    input(
+        Fore.MAGENTA +
+        "\nPress ENTER to return to the menu..."
+    )
